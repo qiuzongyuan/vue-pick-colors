38eaa0ded7746737d6fefbbc618231beba742239
qiuzongyuan <qzy0603@qq.com>
2023-08-11 15:00:54 +0800

    feat:jest测试

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   // u can change this option to a more specific folder for test single component or util when dev
   // for example, ['<rootDir>/packages/input']
-  roots: ['<rootDir>/src']
+  roots: ['<rootDir>/test']
 }
